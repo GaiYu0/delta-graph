a35f0d4c719cbd001829d4f39bed9e5f507d0c7d
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-08-31 18:42:18 +0800

    commit

--- filter.py
+++ filter.py
@@ -3,5 +3,5 @@ import sys
 import pandas as pd
 
 kwargs = eval(sys.argv[1])
-df = pd.read_csv(fileinput.input(), **kwargs)
+df = pd.read_csv(''.join(fileinput.input()), **kwargs)
 print(df.filter(like=eval(sys.argv[2])))
