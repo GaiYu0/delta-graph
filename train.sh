a481cbf2fd1c4334c7223900f3385661a3c0ddd4
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-09-01 10:46:00 +0800

    commit

--- train.sh
+++ train.sh
@@ -1,9 +1,16 @@
-# gpu=0
-# n_gpus=8
+gpu=0
+n_gpus=8
+
+bs_train=100000
+d=512
+n_iters=100
+p_train=0.8
+p_val=0.1
+
 # for i in $(seq 0 9); do
 #     for minus_log_lr in $(seq 1 4); do
 #         for minus_log_wd in $(seq 1 4); do
-#             python3 train.py --bs-infer 100000 --bs-train 100000 --ds ml-10m/partitions/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, 512, r_mean)" --n-iters 100 --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train 0.8 --p-val 0.1 --logdir runs/partition-$i-$minus_log_lr-$minus_log_wd &
+#             python3 train.py --bs-train $bs_train --ds ml-10m/partitions/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --logdir runs/partition-$i-$minus_log_lr-$minus_log_wd &
 #             gpu=$((($gpu + 1) % $n_gpus))
 #             if [ $gpu -eq 0 ]; then
 #                 wait
@@ -12,10 +19,22 @@
 #     done
 # done
 
-gpu=0
-n_gpus=8
 for minus_log_lr in $(seq 1 4); do
     for minus_log_wd in $(seq 1 4); do
-        python3 train_on_snapshot.py --bs-train 100000 --ds ml-10m/snapshots/snapshot-9 --gpu 0 --logdir runs/cbmf-$minus_log_lr-$minus_log_wd --model "CollapsedBiasedMF(n_users, n_items, 512, r_mean)" --n-iters 100 --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train 0.8 --p-val 0.1 --semi
+        python3 train_on_snapshot.py --bs-train $bs_train --ds ml-10m/snapshots/snapshot-9 --gpu $gpu --logdir runs/cbmf/$minus_log_lr-$minus_log_wd --model "CollapsedBiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --semi
+        gpu=$((($gpu + 1) % $n_gpus))
+        if [ $gpu -eq 0 ]; then
+            wait
+        fi
     done
 done
+
+# for minus_log_lr in $(seq 1 4); do
+#     for minus_log_wd in $(seq 1 4); do
+#         python3 train_on_snapshot.py --bs-train $bs_train --ds ml-10m/snapshots/snapshot-9 --gpu $gpu --logdir runs/tbmf/$minus_log_lr-$minus_log_wd --model "TemporalBiasedMF(n_users, n_items, $d, list(map(th.mean, rs_train)), 1)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --semi
+#         gpu=$((($gpu + 1) % $n_gpus))
+#         if [ $gpu -eq 0 ]; then
+#             wait
+#         fi
+#     done
+# done
