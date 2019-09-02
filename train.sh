0da2154df9dffe34259f8a5e1ef9e1748b71320a
Ubuntu <ubuntu@ip-172-31-6-176.ec2.internal>
2019-09-02 04:08:05 +0000

    commit

--- train.sh
+++ train.sh
@@ -10,7 +10,7 @@ p_val=0.1
 # for i in $(seq 0 9); do
 #     for minus_log_lr in $(seq 1 4); do
 #         for minus_log_wd in $(seq 1 4); do
-#             python3 train.py --bs-train $bs_train --ds ml-10m/partitions/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --logdir runs/partition-$i-$minus_log_lr-$minus_log_wd &
+#             python3 train.py --bs-train $bs_train --ds ml-10m/partitions/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --logdir runs/bmf/$i-$minus_log_lr-$minus_log_wd &
 #             gpu=$((($gpu + 1) % $n_gpus))
 #             if [ $gpu -eq 0 ]; then
 #                 wait
