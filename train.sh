5a541e84321607d06d79b11a1d117211c8746611
Ubuntu <ubuntu@ip-172-31-6-176.ec2.internal>
2019-09-02 04:04:07 +0000

    commit

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 gpu=0
 n_gpus=8
 
-bs_train=1000000
-d=512
+bs_train=100000
+d=256
 n_iters=1000
 p_train=0.8
 p_val=0.1
@@ -20,7 +20,6 @@ p_val=0.1
 # done
 
 for minus_log_lr in $(seq 1 4); do
-#   for minus_log_wd in $(seq 1 4); do
     for minus_log_wd in $(seq 5 8); do
         python3 train_on_snapshot.py --bs-train $bs_train --ds ml-10m/snapshots/snapshot-1 --gpu $gpu --logdir runs/cbmf/$minus_log_lr-$minus_log_wd --model "CollapsedBiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --semi &
         gpu=$((($gpu + 1) % $n_gpus))
