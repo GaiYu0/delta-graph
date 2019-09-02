6c78add28ec2468376a25eb04ade94415639969c
Ubuntu <ubuntu@ip-172-31-6-164.ec2.internal>
2019-09-02 11:52:57 +0000

    commit

--- train.sh
+++ train.sh
@@ -7,27 +7,27 @@ n_iters=100
 p_train=0.8
 p_val=0.1
 
-# for i in $(seq 0 9); do
-#     for minus_log_lr in $(seq 1 4); do
-#         for minus_log_wd in $(seq 1 4); do
-#             python3 train.py --bs-train $bs_train --ds ml-10m/partitions/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --logdir runs/bmf/$i-$minus_log_lr-$minus_log_wd &
-#             gpu=$((($gpu + 1) % $n_gpus))
-#             if [ $gpu -eq 0 ]; then
-#                 wait
-#             fi
-#         done
-#     done
-# done
-
-for minus_log_lr in $(seq 1 4); do
-    for minus_log_wd in $(seq 1 4); do
-        python3 train_on_snapshot.py --bs-train $bs_train --ds ml-10m/snapshots-100/snapshot-99 --gpu $gpu --logdir runs/cbmf/$minus_log_lr-$minus_log_wd --model "CollapsedBiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --semi &
-        gpu=$((($gpu + 1) % $n_gpus))
-        if [ $gpu -eq 0 ]; then
-            wait
-        fi
+for i in $(seq 0 99); do
+    for minus_log_lr in $(seq 1 4); do
+        for minus_log_wd in $(seq 1 4); do
+            python3 train.py --bs-train $bs_train --ds ml-10m/partitions-100/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --logdir runs/bmf/$i-$minus_log_lr-$minus_log_wd &
+            gpu=$((($gpu + 1) % $n_gpus))
+            if [ $gpu -eq 0 ]; then
+                wait
+            fi
+        done
     done
 done
+
+# for minus_log_lr in $(seq 1 4); do
+#     for minus_log_wd in $(seq 1 4); do
+#         python3 train_on_snapshot.py --bs-train $bs_train --ds ml-10m/snapshots-100/snapshot-99 --gpu $gpu --logdir runs/cbmf/$minus_log_lr-$minus_log_wd --model "CollapsedBiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --semi &
+#         gpu=$((($gpu + 1) % $n_gpus))
+#         if [ $gpu -eq 0 ]; then
+#             wait
+#         fi
+#     done
+# done
 
 # for minus_log_lr in $(seq 1 4); do
 #     for minus_log_wd in $(seq 1 4); do
