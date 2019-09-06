482c81c166004f9e968aeb932fb8d9fe932e8aad
Gai <giyu@38f9d3588ff5.ant.amazon.com>
2019-09-06 10:38:29 +0800

    commit

---
+++ distributed.sh
@@ -0,0 +1,33 @@
+hosts=(127.0.0.1 127.0.0.2)
+gpu_ptr=($(seq 8 8 $((8 * ${#hosts[*]}))))
+gpus=($(for i in $(seq 1 ${#hosts[*]}); do echo $(seq 0 7); done))
+
+bs_train=100000
+d=256
+n_iters=100
+p_train=0.8
+p_val=0.1
+
+host_idx=0
+gpu_idx=0
+for i in $(seq 0 99); do
+    for minus_log_lr in $(seq 1 4); do
+        for minus_log_wd in $(seq 1 4); do
+            echo ${hosts[$host_idx]} ${gpus[$gpu_idx]}
+#           python3 train.py --bs-train $bs_train --ds ml-10m/partitions-100/partition-$i --gpu $gpu --model "BiasedMF(n_users, n_items, $d, r_mean)" --n-iters $n_iters --optim "Adam(model.parameters(), 1e-$minus_log_lr, weight_decay=1e-$minus_log_wd)" --p-train $p_train --p-val $p_val --logdir runs/bmf/$i-$minus_log_lr-$minus_log_wd > logs/$i-$minus_log_lr-$minus_log_wd &
+            gpu_idx=$(($gpu_idx + 1))
+            if [ $gpu_idx -eq ${gpu_ptr[$host_idx]} ]; then
+                host_idx=$(($host_idx + 1))
+            fi
+            if [ $host_idx -eq ${#hosts[*]} ]; then
+                wait
+                for host in ${hosts[*]}; do
+                    scp -r ubuntu@$host:delta-graph/runs/* runs &
+                done
+                wait
+                host_idx=0
+                gpu_idx=0
+            fi
+        done
+    done
+done
