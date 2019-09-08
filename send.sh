381560c9506d6f69f8c47bea752b8d22735a50ca
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-09-08 11:55:29 +0000

    commit

--- send.sh
+++ send.sh
@@ -1,4 +1,8 @@
-hosts=(3.94.29.70 34.236.254.237 3.222.200.79 34.205.87.121)
+hosts=($3)
+if [ -z $hosts ]; then
+    hosts=($(cat hosts))
+fi
+
 for host in ${hosts[*]}; do
     scp -r $1 ubuntu@$host:$2 &
 done
