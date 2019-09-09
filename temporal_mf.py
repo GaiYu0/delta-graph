d8430a04ef4289a2c55926c2cf880ab4c1226d6a
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-09-09 10:53:09 +0000

    commit

--- temporal_mf.py
+++ temporal_mf.py
@@ -27,12 +27,12 @@ class TemporalBiasedMF(nn.Module):
         self.merge_u = lambda x, h: [x, h]
         self.merge_i = lambda x, h: [x, h]
         '''
+        '''
         self.merge_u = lambda x, h: ([x, x] if h is None else [x + h, x + h])
         self.merge_i = lambda x, h: ([x, x] if h is None else [x + h, x + h])
         '''
         self.merge_u = nn.RNN(d, d)
         self.merge_i = nn.RNN(d, d)
-        '''
         '''
         self.merge_u = nn.LSTM(d, d)
         self.merge_i = nn.LSTM(d, d)
