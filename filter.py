77d92220830cc0b044119cea21b0b239185c5012
Ubuntu <ubuntu@ip-172-31-39-153.ec2.internal>
2019-09-09 09:04:27 +0000

    commit

--- filter.py
+++ filter.py
@@ -1,5 +1,5 @@
 import sys
 import pandas as pd
 
-df = pd.read_csv(sys.stdin, delimiter=' ', header=None, index_col=0)
-print(df.loc[eval(sys.argv[1]), :].values.item())
+df = pd.read_csv(sys.argv[1], delimiter=' ', header=None, index_col=0)
+print(df.loc[eval(sys.argv[2]), :].values.item())
